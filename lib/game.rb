78dfdcef0dbf7d66fb666a68f943324a87e25225
Rene Flooren <floorenr@gmail.com>
2022-01-15 13:44:34 +0100

    Add 'content' key to all board position hashes with value nil

--- lib/game.rb
+++ lib/game.rb
@@ -7,7 +7,7 @@ class Board
     @board_array = []
     (1..8).each do |i|
       ('a'..'h').each do |j|
-        @board_array << { 'pos' => "#{j},#{i}" }
+        @board_array << { 'pos' => "#{j},#{i}", 'content' => nil }
       end
     end
   end
