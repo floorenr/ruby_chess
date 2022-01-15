87f21645eb55d77177a6423ce412b3004a1296e2
Rene Flooren <floorenr@gmail.com>
2022-01-15 13:55:04 +0100

    Split board positions in column and row key/values

--- lib/game.rb
+++ lib/game.rb
@@ -7,8 +7,16 @@ class Board
     @board_array = []
     (1..8).each do |i|
       ('a'..'h').each do |j|
-        @board_array << { 'pos' => "#{j},#{i}", 'content' => nil }
+        @board_array << { 'column' => j 'row' => i , 'content' => nil }
       end
     end
+    # init_pieces
+  end
+
+  # def init_pieces
+  #   @board_array.collect! { |element|
+  #     (element[] == "hello") ? "hi" : element
+  #   }
+  #   puts x
   end
 end
