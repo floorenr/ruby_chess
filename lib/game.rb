4f6887e4c2542dad1150a40214c800ae6fb52ea3
Rene Flooren <floorenr@gmail.com>
2022-01-23 23:12:01 +0100

    Create turn_message to clarify desired user input

--- lib/game.rb
+++ lib/game.rb
@@ -3,6 +3,17 @@ require_relative 'board'
 class Game
   def initialize
     @board = Board.new
+    @cur_player = 'white'
+  end
+
+  def game_loop
+    turn_message
+  end
+
+  def turn_message
+    puts "Player #{@cur_player.upcase}, make your choice"
+    puts "Type the position of desired piece to move (eg. 'a1')"
+    puts "Or type 'c', 'cm', or 's' to declare check, checkmate or save game"
   end
 end
 
--- lib/main.rb
+++ lib/main.rb
@@ -8,4 +8,6 @@ puts "
 
 "
 
-Game.new
+game = Game.new
+
+game.game_loop
