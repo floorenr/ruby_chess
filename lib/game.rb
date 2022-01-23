4d3e9bb005860da9fb4097ea1bc96acd99eee70d
Rene Flooren <floorenr@gmail.com>
2022-01-23 23:53:06 +0100

    Create and initialize functions for other options in turn_message

--- lib/game.rb
+++ lib/game.rb
@@ -5,19 +5,36 @@ class Game
     @board = Board.new
     @cur_player = 'white'
     @prompt = TTY::Prompt.new
+    @selection = nil
   end
 
   def game_loop
     turn_message
+    save_game if @selection == "Save game"
+    check if @selection == "Call 'check'"
+    checkmate if @selection == "Call 'checkmate'"
+    make_move if @selection == "Make a move"
   end
 
   def turn_message
     choices = ["Make a move", "Call 'check'", "Call 'checkmate'", "Save game"]
-    selection = @prompt.select("Player #{@cur_player.upcase}, make your choice", choices)
-    save_game if selection == "Save game"
+    @selection = @prompt.select("Player #{@cur_player.upcase}, make your choice", choices)
+
   end
 
   def save_game
+
+  end
+
+  def make_move
+
+  end
+
+  def check
+
+  end
+
+  def checkmate
     
   end
 end
