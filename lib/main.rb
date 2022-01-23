1cd4fb2703598493f0e5e718d5684698cdd292f6
Rene Flooren <floorenr@gmail.com>
2022-01-23 23:33:20 +0100

    Add option to choose new game or saved game with tty-prompt gem

--- lib/main.rb
+++ lib/main.rb
@@ -1,5 +1,15 @@
 require_relative 'game'
 require_relative 'board'
+require "tty-prompt"
+
+def new_game
+  game = Game.new
+  game.game_loop
+end
+
+def saved_game
+
+end
 
 puts "
 ╔═╗┬ ┬┌─┐┌─┐┌─┐
@@ -7,7 +17,7 @@ puts "
 ╚═╝┴ ┴└─┘└─┘└─┘
 
 "
+prompt = TTY::Prompt.new
+game_choice = prompt.select("Choose your game?", ["New Game", "Saved Game"])
 
-game = Game.new
-
-game.game_loop
+game_choice == "Saved Game"? saved_game : new_game
