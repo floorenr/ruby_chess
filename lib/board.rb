3adcd616a2c7f3d17a25440ad545443af774c28c
Rene Flooren <floorenr@gmail.com>
2022-01-23 17:08:13 +0100

    Give chess pieces own class

--- lib/board.rb
+++ lib/board.rb
@@ -22,26 +22,26 @@ class Board
 
   def create_setup
     @init_setup = [
-      ['a', 1, 'rook'],
-      ['b', 1, 'knight'],
-      ['c', 1, 'bishop'],
-      ['d', 1, 'queen'],
-      ['e', 1, 'king'],
-      ['f', 1, 'bishop'],
-      ['g', 1, 'knight'],
-      ['h', 1, 'rook'],
-      ['a', 8, 'rook'],
-      ['b', 8, 'knight'],
-      ['c', 8, 'bishop'],
-      ['d', 8, 'queen'],
-      ['e', 8, 'king'],
-      ['f', 8, 'bishop'],
-      ['g', 8, 'knight'],
-      ['h', 8, 'rook'],
+      ['a', 1, Rook.new],
+      ['b', 1, Knight.new],
+      ['c', 1, Bishop.new],
+      ['d', 1, Queen.new],
+      ['e', 1, King.new],
+      ['f', 1, Bishop.new],
+      ['g', 1, Knight.new],
+      ['h', 1, Rook.new],
+      ['a', 8, Rook.new],
+      ['b', 8, Knight.new],
+      ['c', 8, Bishop.new],
+      ['d', 8, Queen.new],
+      ['e', 8, King.new],
+      ['f', 8, Bishop.new],
+      ['g', 8, Knight.new],
+      ['h', 8, Rook.new],
     ]
     [*'a'..'h'].each do |column|
-      @init_setup << [column, 2, 'pawn']
-      @init_setup << [column, 7, 'pawn']
+      @init_setup << [column, 2, Pawn.new]
+      @init_setup << [column, 7, Pawn.new]
     end
   end
 
@@ -56,3 +56,40 @@ class Board
     end
   end
 end
+
+class Rook
+  def initialize
+
+  end
+end
+
+class Knight
+  def initialize
+
+  end
+end
+
+class Bishop
+  def initialize
+
+  end
+end
+
+class Queen
+  def initialize
+
+  end
+end
+
+class King
+  def initialize
+
+  end
+end
+
+class Pawn
+  def initialize
+
+  end
+end
+
